9672b253fa011ed75f733d27d135f53a381ff907
Jay Lees <jay.lees@me.com>
2018-09-12 22:12:19 +0100

    Expiry to session manager

--- Mixed/Mixed/Model/PartyConfiguration/SessionManager.swift
+++ Mixed/Mixed/Model/PartyConfiguration/SessionManager.swift
@@ -15,11 +15,28 @@ struct Session {
 
 class SessionManager {
     public static let shared = SessionManager()
+    private let maxExpiry = 28800.0 // 8 hours
+    private let dateFormat = "MM-dd-yyyy HH:mm"
     private let defaultsKey = "CURRENTMIXEDSESSION"
     private init() { }
     
     public func hasActiveSession() -> Bool {
-        return UserDefaults.standard.dictionary(forKey: defaultsKey) != nil
+        guard let session = UserDefaults.standard.object(forKey: defaultsKey) else {
+            Logger.log("No session in defaults", type: .debug)
+            return false
+        }
+        print(session)
+        guard let dict = session as? [String:Any], let start = dict["start"] as? String else {
+            Logger.log("No start found", type: .debug)
+            return false
+        }
+        let startDate = date(of: start)
+        guard startDate > Date().addingTimeInterval(-maxExpiry) else {
+            Logger.log("Start was too long ago... clearing", type: .debug)
+            clearActiveSession()
+            return false
+        }
+        return true
     }
     
     public func getActiveSession() -> Session {
@@ -31,11 +48,27 @@ class SessionManager {
     }
     
     public func setActiveSession(_ session: Session) {
-        let dict: [String:Any] = ["partyID": session.partyID, "type": session.type.rawValue]
+        let dict: [String:Any] = ["partyID": session.partyID,
+                                  "type": session.type.rawValue,
+                                  "start": string(of: Date())]
+        Logger.log("Setting active session for \(session.partyID)", type: .debug)
         UserDefaults.standard.set(dict, forKey: defaultsKey)
     }
     
     public func clearActiveSession() {
+        Logger.log("Clearing active session", type: .debug)
         UserDefaults.standard.removeObject(forKey: defaultsKey)
+    }
+    
+    private func string(of date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = dateFormat
+        return dateFormatter.string(from: date)
+    }
+    
+    private func date(of string: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = dateFormat
+        return dateFormatter.date(from: string)!
     }
 }
